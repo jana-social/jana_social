73c7db8c9fa42dce4ad3e9bf45c816b8790ad808
Steven Stackhouse <stackmm@gmail.com>
2023-07-28 11:59:49 -0600

    small refactor

--- spec/requests/api/v1/friendships_spec.rb
+++ spec/requests/api/v1/friendships_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Friendship, type: :request do
-  describe 'POST /api/v1/friendships' do
+  # describe 'POST /api/v1/friendships' do
     
-  end
+  # end
 end
