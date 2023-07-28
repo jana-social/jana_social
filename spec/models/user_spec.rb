91d575172579c6ef63833f34d043643aae0adc28
Steven Stackhouse <stackmm@gmail.com>
2023-07-27 22:10:31 -0600

    add relationships to user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'relationships' do
+    it { should have_many(:friendships).dependent(:destroy) }
+    it { should have_many(:friends).through(:friendships) }
+  end
 end
+
