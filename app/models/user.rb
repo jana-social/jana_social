c2305cc29b01b1ae7ca2ebfc245957307ef8e2a0
Lauren Golden <llgolden@gmail.com>
2023-07-28 08:42:55 -0600

    Updated user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  validates :username, presence: true
+  validates :email, presence: true
+  validates :password_digest, presence: true
+  validates :zipcode, presence: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it { should validate_presence_of(:username) }
+    it { should validate_presence_of(:email) }
+    it { should validate_presence_of(:password_digest) }
+    it { should validate_presence_of(:zipcode) }
+  end
+  # pending "add some examples to (or delete) #{__FILE__}"
 end
