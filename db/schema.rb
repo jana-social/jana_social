b408fcbdfc23d07ff1ee6160c7f413eec4b4dda3
Steven Stackhouse <stackmm@gmail.com>
2023-07-27 22:09:41 -0600

    add friendship tables to schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_27_195847) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_28_023324) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -39,6 +39,16 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_27_195847) do
     t.index ["user_id"], name: "index_events_on_user_id"
   end
 
+  create_table "friendships", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "friend_id", null: false
+    t.string "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["friend_id"], name: "index_friendships_on_friend_id"
+    t.index ["user_id"], name: "index_friendships_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email"
@@ -58,4 +68,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_27_195847) do
   add_foreign_key "event_users", "events", column: "events_id"
   add_foreign_key "event_users", "users", column: "users_id"
   add_foreign_key "events", "users"
+  add_foreign_key "friendships", "users"
+  add_foreign_key "friendships", "users", column: "friend_id"
 end
