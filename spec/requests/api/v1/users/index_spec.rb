2bc9ca0586cae2fe23ff5a7401dd037def93faa5
Andy Weissman <andyweissman6@gmail.com>
2023-07-28 12:57:13 -0600

    create test for users_index_request

---
+++ spec/requests/api/v1/users/index_spec.rb
@@ -0,0 +1,31 @@
+require "rails_helper"
+
+describe "Users index request" do
+  let!(:user_1) { create(:user) }
+  let!(:user_2) { create(:user) }
+  
+  it "requests all the users", vcr: { match_requests_on: [:method, :host, :path] } do
+    get "/api/v1/users"
+    expect(response).to be_successful
+
+    users = JSON.parse(response.body, symbolize_names: true)
+
+    users[:data].each do |user|
+      expect(user).to have_key(:id)
+      expect(user).to have_key(:type)
+      expect(user).to have_key(:attributes)
+
+      expect(user[:attributes]).to have_key(:username)
+      expect(user[:attributes]).to have_key(:email)
+      expect(user[:attributes]).to have_key(:password_digest)
+      expect(user[:attributes]).to have_key(:zipcode)
+      expect(user[:attributes]).to have_key(:street_address)
+      expect(user[:attributes]).to have_key(:bio)
+      expect(user[:attributes]).to have_key(:likes)
+      expect(user[:attributes]).to have_key(:dislikes)
+      expect(user[:attributes]).to have_key(:profile_image_link)
+      expect(user[:attributes]).to have_key(:latitude)
+      expect(user[:attributes]).to have_key(:longitude)
+    end
+  end
+end
