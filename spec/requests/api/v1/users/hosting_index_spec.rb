b2335ee3e6755ca9d078be362885fdb5dde49a41
Jesse Thomas <jgthomas3@gmail.com>
2023-07-29 00:11:58 -0600

    Test: Add assertions to hosting response.

--- spec/requests/api/v1/users/hosting_index_spec.rb
+++ spec/requests/api/v1/users/hosting_index_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-RSpec.describe "Hosting Response" do
+RSpec.describe "Hosting Response", type: :request do
 
   let!(:user_1) { create(:user, username: "Billy Villian", street_address: "990 Summit Blvd.", zipcode: "92315") }
   let!(:user_2) { create(:user, username: "Andrew Rickford", street_address: "1234 Main St.", zipcode: "93109") }
@@ -15,6 +15,7 @@ RSpec.describe "Hosting Response" do
 
       event_user_1 = EventUser.create!(user_id: user_1.id, event_id: event_3.id, status: 2)
       event_user_2 = EventUser.create!(user_id: user_1.id, event_id: event_2.id, status: 2)
+      event_user_3 = EventUser.create!(user_id: user_2.id, event_id: event_1.id, status: 2)
 
       get "/api/v1/users/#{user_1.id}/events/hosting"
 
@@ -23,8 +24,9 @@ RSpec.describe "Hosting Response" do
       expect(hosting[:data].count).to eq(2)
 
       hosting[:data].each do |event|
-
         expect(event).to have_key(:id)
+        expect(event).to have_key(:type)
+        expect(event).to have_key(:attributes)
         expect(event[:attributes]).to have_key(:title)
         expect(event[:attributes]).to have_key(:description)
         expect(event[:attributes]).to have_key(:street_address)
@@ -33,6 +35,17 @@ RSpec.describe "Hosting Response" do
         expect(event[:attributes]).to have_key(:user_id)
         expect(event[:attributes]).to have_key(:date_time)
         expect(event[:attributes]).to have_key(:host)
+
+        expect(event[:attributes][:title]).to be_a(String)
+        expect(event[:attributes][:description]).to be_a(String)
+        expect(event[:attributes][:street_address]).to be_a(String)
+        expect(event[:attributes][:private_status]).to be_in([true, false])
+        expect(event[:attributes][:zipcode]).to be_a(String)
+        expect(event[:attributes][:user_id]).to be_an(Integer)
+        expect(event[:attributes][:date_time]).to be_an(String)
+        expect(event[:attributes][:host]).to be_an(String)
+
+        expect(event[:attributes][:title]).to_not eq(event_2.title)
       end
     end
   end
