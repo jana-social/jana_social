e3536e1e40839a8b849ba289a31a64f652802edf
Steven Stackhouse <stackmm@gmail.com>
2023-07-29 14:23:42 -0600

    refactor layout of user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,19 +1,20 @@
 class User < ApplicationRecord
   has_many :event_users
   has_many :events, through: :event_users
-
+  has_many :friendships, dependent: :destroy
+  has_many :friends, through: :friendships
+  
   before_validation :geocode
 
   validates :username, uniqueness: true, presence: true
   validates :email, uniqueness: true, presence: true
   validates :password_digest, presence: true
   validates :zipcode, presence: true
-  
-  has_many :friendships, dependent: :destroy
-  has_many :friends, through: :friendships
+
 
   geocoded_by :address
   has_secure_password
+
   private
 
   def address
