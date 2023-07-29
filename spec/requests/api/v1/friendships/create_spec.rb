804aeb6ec13583e6aa7a696a9f2586d11824cb4a
Steven Stackhouse <stackmm@gmail.com>
2023-07-29 16:29:24 -0600

    add tests for friendship creation and approval

--- spec/requests/api/v1/friendships/create_spec.rb
+++ spec/requests/api/v1/friendships/create_spec.rb
@@ -4,9 +4,53 @@ RSpec.describe "Create Friendship API" do
   let!(:user_1) { create(:user, username: "Foo", zipcode: "92315", email: "foo@gmail.com", password_digest: "test123") }
   let!(:user_2) { create(:user, username: "Barr",  zipcode: "92315", email: "barr@gmail.com", password_digest: "test123") }
   
-  describe "/api/v1/users/:user_id/friendships" do
-    it "can create a new friendship api", :vcr do
-      require 'pry'; binding.pry
+  describe "/api/v1/friendships" do
+    it "can create a new pending friendship", :vcr do
+      expect(Friendship.count).to eq(0)
+      
+      friendship_params = ({
+        user_id: user_1.id,
+        friend_id: user_2.id,
+        status: "approved"
+      })
+      
+      headers = {"CONTENT_TYPE" => "application/json"}
+      post "/api/v1/friendships", headers: headers, params: JSON.generate(friendship_params)
+      
+      friendship = Friendship.last
+
+      expect(response).to be_successful
+      expect(response.status).to eq(201)
+      
+      expect(friendship.user_id).to eq(friendship_params[:user_id])
+      expect(friendship.friend_id).to eq(friendship_params[:friend_id])
+      expect(friendship.status).to eq("pending")
+      
+      expect(Friendship.count).to eq(1)
+    end
+
+    it "can create a new approved friendship", :vcr do
+      Friendship.create!(user_id: user_1.id, friend_id: user_2.id, status: "pending")
+      
+      friendship_params = ({
+        user_id: user_2.id,
+        friend_id: user_1.id,
+        status: "approved"
+      })
+      
+      headers = {"CONTENT_TYPE" => "application/json"}
+      post "/api/v1/friendships", headers: headers, params: JSON.generate(friendship_params)
+      
+      friendship = Friendship.last
+
+      expect(response).to be_successful
+      expect(response.status).to eq(201)
+      
+      expect(friendship.user_id).to eq(friendship_params[:friend_id])
+      expect(friendship.friend_id).to eq(friendship_params[:user_id])
+      expect(friendship.status).to eq("approved")
+      
+      expect(Friendship.count).to eq(1)
     end
   end
 end
