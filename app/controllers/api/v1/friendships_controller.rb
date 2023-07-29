cbc0bb37ea3b15d0aa248ee54aeb2340b781fea5
Steven Stackhouse <stackmm@gmail.com>
2023-07-29 16:29:56 -0600

    refactor friendship controller create method

--- app/controllers/api/v1/friendships_controller.rb
+++ app/controllers/api/v1/friendships_controller.rb
@@ -1,10 +1,11 @@
 class Api::V1::FriendshipsController < ApplicationController
-
-  # POST /friendships
   def create
+    initiator = User.find(params[:user_id])
     receiver = User.find(params[:friend_id])
     status = params[:status].to_sym
-    friendship = Friendship.process_friendship(initiator, receiver, status)
+
+    Friendship.process_friendship(initiator, receiver, status)
+    friendship = Friendship.find_friendship(initiator, receiver)
     render json: FriendshipSerializer.new(friendship), status: :created
   end
 end
