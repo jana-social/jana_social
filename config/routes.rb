72e4f5ac310ef40621e9d97415aa706b02c73db0
Lauren Golden <llgolden@gmail.com>
2023-07-29 13:13:36 -0600

    Resolve merge conflicts

--- config/routes.rb
+++ config/routes.rb
@@ -10,11 +10,10 @@ Rails.application.routes.draw do
       get "/users/:id/events/hosting", to: "users/events#hosting"
       get "/users/:id/events/attending", to: "users/events#attending"
       delete "/users/:user_id/events/:id", to: "events#delete"
-      resources :users do
+      resources :users, only: %i[index show create update destroy] do
         resources :events, only: %i[create update]
-      end, only: %i[index show create update destroy]
+      end
       resources :events, only: %i[index show]
     end
   end
-
 end
