903d2081f376bfa64bd2a3f7e954d908ec39fa91
Jesse Thomas <jgthomas3@gmail.com>
2023-07-29 00:13:29 -0600

    Feat: Add attending method

--- app/controllers/api/v1/users/events_controller.rb
+++ app/controllers/api/v1/users/events_controller.rb
@@ -4,4 +4,10 @@ class Api::V1::Users::EventsController < ApplicationController
 
    render json: EventShowSerializer.new(hosting)
   end
+
+  def attending
+    events = User.find(params[:id]).events
+
+    render json: EventShowSerializer.new(events)
+  end
 end
