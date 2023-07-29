c98332b1d5ffc1ccba333ce23859b04958523c35
Steven Stackhouse <stackmm@gmail.com>
2023-07-29 16:36:52 -0600

    refactor process_friendship with more consistent syntax

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -2,13 +2,13 @@ class Friendship < ApplicationRecord
   belongs_to :user
   belongs_to :friend, class_name: 'User', foreign_key: 'friend_id'
 
-  def self.process_friendship(initiator, receiver, status)
-    friendship = Friendship.find_by(user: initiator, friend: receiver)
-    reverse_friendship = Friendship.find_by(user: receiver, friend: initiator) 
+  def self.process_friendship(user, friend, status)
+    friendship = Friendship.find_by(user: user, friend: friend)
+    reverse_friendship = Friendship.find_by(user: friend, friend: user) 
     
     if friendship.nil? && reverse_friendship.nil? # if a friendship does NOT exist
       friendship_status = status == :approved ? :pending : :declined
-      Friendship.create(user: initiator, friend: receiver, status: friendship_status)
+      Friendship.create(user: user, friend: friend, status: friendship_status)
     elsif reverse_friendship&.status == 'pending' # if a friendship does exist (in the reverse order) and the status is pending
       reverse_friendship.update(status: :approved) if status == :approved
       reverse_friendship.update(status: :declined) if status == :declined
