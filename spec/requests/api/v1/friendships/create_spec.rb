9aebb3208dd9495f3b5159c8ab8260c3cecd3547
Steven Stackhouse <stackmm@gmail.com>
2023-07-29 19:42:33 -0600

    update vcrs

--- spec/requests/api/v1/friendships/create_spec.rb
+++ spec/requests/api/v1/friendships/create_spec.rb
@@ -5,52 +5,104 @@ RSpec.describe "Create Friendship API" do
   let!(:user_2) { create(:user, username: "Barr",  zipcode: "92315", email: "barr@gmail.com", password_digest: "test123") }
   
   describe "/api/v1/friendships" do
-    it "can create a new pending friendship", :vcr do
-      expect(Friendship.count).to eq(0)
-      
-      friendship_params = ({
-        user_id: user_1.id,
-        friend_id: user_2.id,
-        status: "approved"
-      })
-      
-      headers = {"CONTENT_TYPE" => "application/json"}
-      post "/api/v1/friendships", headers: headers, params: JSON.generate(friendship_params)
-      
-      friendship = Friendship.last
+    context "when no previous friendship exists between two users" do
+      it "can create a new pending friendship (first user approves)", :vcr do
+        expect(Friendship.count).to eq(0)
+        
+        friendship_params = ({
+          user_id: user_1.id,
+          friend_id: user_2.id,
+          status: "approved"
+        })
+        
+        headers = {"CONTENT_TYPE" => "application/json"}
+        post "/api/v1/friendships", headers: headers, params: JSON.generate(friendship_params)
+        
+        friendship = Friendship.last
+  
+        expect(response).to be_successful
+        expect(response.status).to eq(201)
+        
+        expect(friendship.user_id).to eq(friendship_params[:user_id])
+        expect(friendship.friend_id).to eq(friendship_params[:friend_id])
+        expect(friendship.status).to eq("pending")
+        
+        expect(Friendship.count).to eq(1)
+      end
 
-      expect(response).to be_successful
-      expect(response.status).to eq(201)
-      
-      expect(friendship.user_id).to eq(friendship_params[:user_id])
-      expect(friendship.friend_id).to eq(friendship_params[:friend_id])
-      expect(friendship.status).to eq("pending")
-      
-      expect(Friendship.count).to eq(1)
+      it "can create a new declined friendship (first user declines)", :vcr do
+        expect(Friendship.count).to eq(0)
+        
+        friendship_params = ({
+          user_id: user_1.id,
+          friend_id: user_2.id,
+          status: "declined"
+        })
+        
+        headers = {"CONTENT_TYPE" => "application/json"}
+        post "/api/v1/friendships", headers: headers, params: JSON.generate(friendship_params)
+        
+        friendship = Friendship.last
+  
+        expect(response).to be_successful
+        expect(response.status).to eq(201)
+        
+        expect(friendship.user_id).to eq(friendship_params[:user_id])
+        expect(friendship.friend_id).to eq(friendship_params[:friend_id])
+        expect(friendship.status).to eq("declined")
+        
+        expect(Friendship.count).to eq(1)
+      end
     end
 
-    it "can create a new approved friendship", :vcr do
-      Friendship.create!(user_id: user_1.id, friend_id: user_2.id, status: "pending")
-      
-      friendship_params = ({
-        user_id: user_2.id,
-        friend_id: user_1.id,
-        status: "approved"
-      })
-      
-      headers = {"CONTENT_TYPE" => "application/json"}
-      post "/api/v1/friendships", headers: headers, params: JSON.generate(friendship_params)
-      
-      friendship = Friendship.last
+    context "when a previous pending friendship exists between two users" do
+      it "can create a new approved friendship (second user approves)", :vcr do
+        Friendship.create!(user_id: user_1.id, friend_id: user_2.id, status: "pending")
+        
+        friendship_params = ({
+          user_id: user_2.id,
+          friend_id: user_1.id,
+          status: "approved"
+        })
+        
+        headers = {"CONTENT_TYPE" => "application/json"}
+        post "/api/v1/friendships", headers: headers, params: JSON.generate(friendship_params)
+        
+        friendship = Friendship.last
+  
+        expect(response).to be_successful
+        expect(response.status).to eq(201)
+        
+        expect(friendship.user_id).to eq(friendship_params[:friend_id])
+        expect(friendship.friend_id).to eq(friendship_params[:user_id])
+        expect(friendship.status).to eq("approved")
+        
+        expect(Friendship.count).to eq(1)
+      end
 
-      expect(response).to be_successful
-      expect(response.status).to eq(201)
-      
-      expect(friendship.user_id).to eq(friendship_params[:friend_id])
-      expect(friendship.friend_id).to eq(friendship_params[:user_id])
-      expect(friendship.status).to eq("approved")
-      
-      expect(Friendship.count).to eq(1)
+      it "can create a new declined friendship (second user declines)", :vcr do
+        Friendship.create!(user_id: user_1.id, friend_id: user_2.id, status: "pending")
+        
+        friendship_params = ({
+          user_id: user_2.id,
+          friend_id: user_1.id,
+          status: "declined"
+        })
+        
+        headers = {"CONTENT_TYPE" => "application/json"}
+        post "/api/v1/friendships", headers: headers, params: JSON.generate(friendship_params)
+        
+        friendship = Friendship.last
+
+        expect(response).to be_successful
+        expect(response.status).to eq(201)
+        
+        expect(friendship.user_id).to eq(friendship_params[:friend_id])
+        expect(friendship.friend_id).to eq(friendship_params[:user_id])
+        expect(friendship.status).to eq("declined")
+        
+        expect(Friendship.count).to eq(1)
+      end
     end
   end
 end
