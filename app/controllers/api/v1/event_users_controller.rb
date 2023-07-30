93812f47676da3e90e5ead24ca6393fbf95e419e
Jesse Thomas <jgthomas3@gmail.com>
2023-07-30 13:10:28 -0600

    Feat: Establish EventUsersController

---
+++ app/controllers/api/v1/event_users_controller.rb
@@ -0,0 +1,8 @@
+class Api::V1::EventUsersController < ApplicationController
+  def index
+    attendees = Event.find(params[:event_id]).event_users
+
+    render json: EventUserSerializer.new(attendees)
+
+  end
+end
