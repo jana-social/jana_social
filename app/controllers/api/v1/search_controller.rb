40a6c5b7cad0904751335f2d8e757777fdccd5fe
Garrett Gregor <garrett.gregor@gmail.com>
2023-07-31 14:57:14 -0600

    add authentication for frontend

--- app/controllers/api/v1/search_controller.rb
+++ app/controllers/api/v1/search_controller.rb
@@ -4,14 +4,10 @@ module Api
       def find
         user = User.find_by(email: params[:q])
         if user&.authenticate(params[:p])
-          # session[:user_id] = user.id
           render json: UserSerializer.new(user)
         else
           render json: { errors: ["Invalid username or password"] }, status: :unauthorized
         end
-        # user = User.search_by_email(params[:q])
-        # if user.auth
-        # render json: UserSerializer.new(user)
       end
     end
   end
