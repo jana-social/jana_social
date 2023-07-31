579dfd6affdb3ebba66b285806cb8b92cf463dc2
Garrett Gregor <garrett.gregor@gmail.com>
2023-07-31 14:55:36 -0600

    rescue from records not found

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::API
+  rescue_from ActiveRecord::RecordNotFound, with: :error_response
+
+  def error_response(error)
+    render json: ErrorSerializer.new(error).serialize_json, status: :not_found
+  end
+
   def welcome
   end
 end
