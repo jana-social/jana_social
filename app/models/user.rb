c308a66e8f75dedea22095030a59277180e2959a
Steven Stackhouse <stackmm@gmail.com>
2023-08-01 00:30:22 -0600

    add first version of potential_friends

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,9 +17,10 @@ class User < ApplicationRecord
   # returns all users that have a pending friendship with that user (and are waiting for that user to respond)
   def pending_friends
     User.joins(:friendships).where(friendships: { status: "pending", friend_id: id })
+    # self.friendships.where(status: "pending")
   end
 
-  # returns all unique users that the current user has an approved friendship with
+  # returns all users that the current user has an approved friendship with
   def approved_friends
     friendships_as_user = self.friendships.where(status: "approved").map(&:friend)
     friendships_as_friend = Friendship.where(friend: self, status: "approved").map(&:user)
@@ -33,6 +34,20 @@ class User < ApplicationRecord
     (friendships_as_user + friendships_as_friend).uniq
   end
 
+  # returns all users that the current user could approve or deny for friendship
+  # i.e. all users that are not the current user, 
+  # are not declined friends with the current user,
+  # are not already approved friends with the current user, 
+  # but DO include the users that are waiting for the current user to respond to a pending friendship
+  def potential_friends
+    my_pending_friends = User.joins(:friendships).where(friendships: { status: "pending", user_id: id })
+
+    pending_friendships_as_user = self.friendships.where(status: "pending").map(&:friend)
+    
+    excluded_users = (pending_friendships_as_user + my_pending_friends + declined_friends + approved_friends + [self]).map(&:id)
+
+    User.where.not(id: excluded_users)
+  end
   private
 
   def address
