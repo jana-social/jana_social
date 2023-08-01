5f6db03e3c971f7e7fcb7afe2256989b8d566337
Steven Stackhouse <stackmm@gmail.com>
2023-08-01 00:40:32 -0600

    refactor for clarity

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,7 +17,6 @@ class User < ApplicationRecord
   # returns all users that have a pending friendship with that user (and are waiting for that user to respond)
   def pending_friends
     User.joins(:friendships).where(friendships: { status: "pending", friend_id: id })
-    # self.friendships.where(status: "pending")
   end
 
   # returns all users that the current user has an approved friendship with
@@ -35,15 +34,9 @@ class User < ApplicationRecord
   end
 
   # returns all users that the current user could approve or deny for friendship
-  # i.e. all users that are not the current user, 
-  # are not declined friends with the current user,
-  # are not already approved friends with the current user, 
-  # but DO include the users that are waiting for the current user to respond to a pending friendship
   def potential_friends
     my_pending_friends = User.joins(:friendships).where(friendships: { status: "pending", user_id: id })
-
     pending_friendships_as_user = self.friendships.where(status: "pending").map(&:friend)
-    
     excluded_users = (pending_friendships_as_user + my_pending_friends + declined_friends + approved_friends + [self]).map(&:id)
 
     User.where.not(id: excluded_users)
