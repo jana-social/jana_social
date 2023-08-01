6a7f572574e06c75a8c8497b7209cd21b1040a63
Steven Stackhouse <stackmm@gmail.com>
2023-08-01 16:23:30 -0600

    create users search controller and index method

---
+++ app/controllers/api/v1/users/search_controller.rb
@@ -0,0 +1,10 @@
+class Api::V1::Users::SearchController < ApplicationController
+  def index
+    if params[:distance].present?
+      users = User.potential_friends_by_distance(params[:distance])
+      render json: FriendSerializer.new(users)
+    else
+      render json: { error: 'No results found' }, status: :not_found
+    end
+  end
+end
