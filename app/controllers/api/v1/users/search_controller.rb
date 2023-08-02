d007dff7294b79ea5328b8325ceab41d375829d0
Steven Stackhouse <stackmm@gmail.com>
2023-08-02 10:24:09 -0600

    rubocop refactor

--- app/controllers/api/v1/users/search_controller.rb
+++ app/controllers/api/v1/users/search_controller.rb
@@ -1,11 +1,11 @@
 class Api::V1::Users::SearchController < ApplicationController
   def index
     user = User.find(params[:id])
-    if params[:distance].present? && params[:distance].to_i > 0
+    if params[:distance].present? && params[:distance].to_i.positive?
       potential_friends = user.potential_friends_nearby(params[:distance])
       render json: FriendSerializer.new(potential_friends)
     else
-      render json: { error: 'No results found' }, status: :not_found
+      render json: { error: "No results found" }, status: :not_found
     end
   end
 end
