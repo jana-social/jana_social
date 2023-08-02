0bd8fd03dc52f5a846e0ca42ea45f01d984d097a
Steven Stackhouse <stackmm@gmail.com>
2023-08-02 09:35:50 -0600

    modify GET request per FE request

--- spec/requests/api/v1/users/find_friends_spec.rb
+++ spec/requests/api/v1/users/find_friends_spec.rb
@@ -7,17 +7,12 @@ RSpec.describe "Find Friends API", type: :request do
 
   describe "GET /api/v1/users/:id/find_friends" do
     it "returns a list of the user's possible friends based on search radius" do
-      Friendship.process_friendship(@user1, @user2, :approved)
-      Friendship.process_friendship(@user2, @user1, :approved)
-      Friendship.process_friendship(@user1, @user3, :approved)
-      Friendship.process_friendship(@user3, @user1, :approved)
-      Friendship.process_friendship(@user1, @user4, :approved)
-      Friendship.process_friendship(@user4, @user1, :approved)
-
-      get "/api/v1/users/#{@user1.id}/find_friends", params: { distance: 50 }
+      get "/api/v1/users/#{@user1.id}/find_friends?distance=50"
 
       expect(response).to be_successful
       expect(response.status).to eq(200)
+
+      friends = JSON.parse(response.body, symbolize_names: true)
 
       expect(friends[:data].count).to eq(1)
 
@@ -26,6 +21,7 @@ RSpec.describe "Find Friends API", type: :request do
       expect(friends[:data].first).to have_key(:attributes)
 
       expect(friends[:data].first[:attributes]).to have_key(:username)
+      expect(friends[:data].first[:attributes][:username]).to eq(@user2.username)
       expect(friends[:data].first[:attributes]).to have_key(:bio)
       expect(friends[:data].first[:attributes]).to have_key(:likes)
       expect(friends[:data].first[:attributes]).to have_key(:dislikes)
