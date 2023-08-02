08288b7e951439b0f9d5b53d5e55ed9a0eeb606d
Steven Stackhouse <stackmm@gmail.com>
2023-08-02 09:35:06 -0600

    finish index method logic

--- app/controllers/api/v1/users/search_controller.rb
+++ app/controllers/api/v1/users/search_controller.rb
@@ -1,8 +1,9 @@
 class Api::V1::Users::SearchController < ApplicationController
   def index
-    if params[:distance].present?
-      users = User.potential_friends_nearby(params[:distance])
-      render json: FriendSerializer.new(users)
+    user = User.find(params[:id])
+    if params[:distance].present? && params[:distance].to_i > 0
+      potential_friends = user.potential_friends_nearby(params[:distance])
+      render json: FriendSerializer.new(potential_friends)
     else
       render json: { error: 'No results found' }, status: :not_found
     end
