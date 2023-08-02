d3eb2c10ecdd8301696a74c6fe72973034d7f20a
Garrett Gregor <garrett.gregor@gmail.com>
2023-08-02 11:48:16 -0600

    test: manually create data

--- spec/serializers/users_serializer_spec.rb
+++ spec/serializers/users_serializer_spec.rb
@@ -3,7 +3,14 @@ require "rails_helper"
 describe UserSerializer do
   it "should not include password data" do
     VCR.use_cassette("openstreetmap_api") do
-      user = create(:user, password: "secret_password")
+      user = User.create!(
+        username: "Foo",
+        zipcode: "80203",
+        street_address: "505 E Colfax Ave",
+        email: "foo@gmail.com",
+        password: "test123"
+      )
+
       serialized_user = described_class.new(user).as_json
       expect(serialized_user).not_to have_key(:password)
     end
