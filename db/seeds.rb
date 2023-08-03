160edf04fa4db0388fbdc2c6c088beca85518274
Garrett Gregor <garrett.gregor@gmail.com>
2023-08-03 12:54:08 -0600

    fix seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,17 +10,15 @@ User.destroy_all
 Event.destroy_all
 EventUser.destroy_all
 
-# <<<<<<<<< Temporary merge branch 1
 user_1 = User.create!(
   username: "Sweet Bread",
   email: "stefanie@jaskolski-sanford.example",
   password: "test",
-  zipcode: "01417",
-  street_address: "13575 Lavonda Ways",
+  zipcode: "80301",
+  street_address: "1800 28th St",
   bio: "[\"Five dollar toast sriracha pop-up.\", \"Williamsburg austin mustache kombucha literally ennui pabst.\", \"Umami post-ironic direct trade.\", \"Sustainable diy park seitan pop-up selvage farm-to-table cold-pressed.\", \"Roof yolo blog.\"]",
   likes: "Gentrify roof hoodie mlkshk skateboard.",
-  dislikes: "Lo-fi irony you probably haven't heard of them fingerstache tofu.",
-  profile_image_link: "http://cummings-murphy.test/kim_hoppe"
+  dislikes: "Lo-fi irony you probably haven't heard of them fingerstache tofu."
 )
 
 user_2 = User.create!(
@@ -28,7 +26,6 @@ user_2 = User.create!(
   email: "carlton.padberg@bogisich.example",
   password: "test",
   zipcode: "24601-5905",
-  street_address: "9747 Agripina Mews",
   bio: "[\"Freegan diy fanny pack.\", \"Ethical slow-carb photo booth cardigan.\", \"Seitan williamsburg viral deep v tattooed.\", \"Waistcoat wolf ramps church-key.\", \"Roof cleanse chia pop-up wes anderson.\"]",
   likes: "Tattooed hammock street keytar.",
   dislikes: "Muggle magic everyday selfies humblebrag messenger bag organic listicle swag."
@@ -39,7 +36,6 @@ user_3 = User.create!(
   email: "nicholle_stroman@feeney-mraz.test",
   password: "test",
   zipcode: "42472-6277",
-  street_address: "93085 Thompson Ramp",
   bio: "[\"Distillery cardigan ennui fanny pack ethical tilde taxidermy yr.\", \"Plaid franzen iphone cred.\", \"Squid gentrify wayfarers ethical austin.\", \"Cliche migas kinfolk try-hard hoodie.\", \"Fashion axe helvetica yuccie.\"]",
   likes: "Plaid fashion axe swag carry.",
   dislikes: "Kinfolk umami pitchfork biodiesel deep v."
@@ -53,10 +49,7 @@ user_4 = User.create!(
   street_address: nil,
   bio: "I am Billy!",
   likes: ["walks", "beaches", "beach-walks"],
-  dislikes: ["sand", "water", "legs"],
-  profile_image_link: "https://unsplash.com/photos/aQSPWSxCweY",
-  latitude: nil,
-  longitude: nil
+  dislikes: ["sand", "water", "legs"]
 )
 
 user_5 = User.create!(
@@ -64,7 +57,6 @@ user_5 = User.create!(
   email: "jeremiah@grimes.example",
   password: "test",
   zipcode: "79859-1323",
-  street_address: "1744 Arden Walk",
   bio: "[\"Vice leggings post-ironic kombucha organic cronut try-hard master.\", \"Typewriter tilde gentrify meditation.\", \"Cronut master intelligentsia.\", \"Park listicle photo booth trust fund craft beer dreamcatcher seitan.\", \"Chia kombucha craft beer green juice umami tousled flexitarian.\"]",
   likes: "Mixtape 90's muggle magic selfies godard asymmetrical fashion axe schlitz.",
   dislikes: "Williamsburg authentic helvetica polaroid blue bottle waistcoat brooklyn five dollar toast."
@@ -81,6 +73,3 @@ event_1 = Event.create!(
   longitude: nil,
   user_id: 1
 )
-
-# rake db:seed RAILS_ENV=production
-
