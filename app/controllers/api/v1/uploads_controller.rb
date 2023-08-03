a644a11d231becc8ce8eaa389a6f4056e29f9f78
Lauren Golden <llgolden@gmail.com>
2023-08-03 15:41:47 -0600

    WIP

--- app/controllers/api/v1/uploads_controller.rb
+++ app/controllers/api/v1/uploads_controller.rb
@@ -10,7 +10,7 @@ module Api
         Aws.config.update(access_key_id: ENV["ACCESS_KEY"], secret_access_key: ENV["SECRET_ACCESS_KEY"])
         bucket = Aws::S3::Resource.new.bucket(ENV["BUCKET_NAME"])
         
-        require 'pry'; binding.pry
+        # require 'pry'; binding.pry
         file = bucket.object(params[:file][:original_filename])
         
         file.upload_file(params[:file][:headers], acl: 'public-read')
