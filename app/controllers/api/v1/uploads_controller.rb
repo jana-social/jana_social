5cc4c6c3a9cc39bfd43b0645490be8c8482d15f8
Andy Weissman <andyweissman6@gmail.com>
2023-08-03 19:58:34 -0600

    get photo to appear on AWS and not be empty

--- app/controllers/api/v1/uploads_controller.rb
+++ app/controllers/api/v1/uploads_controller.rb
@@ -5,26 +5,24 @@ module Api
         @uploads = Upload.all
       end
 
-      # POST /uploads
       def create
         Aws.config.update(access_key_id: ENV["ACCESS_KEY"], secret_access_key: ENV["SECRET_ACCESS_KEY"])
         bucket = Aws::S3::Resource.new.bucket(ENV["BUCKET_NAME"])
-        
-        # require 'pry'; binding.pry
-        file = bucket.object(params[:file][:original_filename])
-        
-        file.upload_file(params[:file][:headers], acl: 'public-read')
 
-        # file.upload_file(params[:file], acl: 'public-read')
-        # file.put(acl: "public-read")
-        # file.put(params[:file], acl: 'public-read')
-        # file.upload_file(file, acl: "public-read")
-        
-        
-        photo = Upload.create(link: file.public_url)
-        redirect_to api_v1_users_update_path(photo.profile_image_link)
+        file = params[:file] # The file parameter will be an instance of ActionDispatch::Http::UploadedFile
+
+        # Upload the file to AWS S3 bucket
+        file_path = "uploads/#{SecureRandom.uuid}/#{file.original_filename}"
+        file_obj = bucket.object(file_path)
+        file_obj.upload_file(file.tempfile, acl: "public-read")
+
+        photo = Upload.create(link: file_obj.public_url)
+
+        render json: { data: { profile_image_link: photo.link } }, status: :ok
+      rescue StandardError => e
+        render json: { error: "Failed to upload file: #{e.message}" }, status: :unprocessable_entity
       end
-
+      
       def new
       end
     end
--- spec/requests/api/v1/uploads/receive_upload_spec.rb
+++ spec/requests/api/v1/uploads/receive_upload_spec.rb
@@ -5,24 +5,17 @@ RSpec.describe "Uploads API", type: :request do
   describe "POST /api/v1/uploads" do
     describe "happy path" do
       it "returns user info with updated AWS profile image link" do
-        file_info = "{\"tempfile\":\"#\\u003cFile:0x0000000115838608\\u003e\",\"content_type\":\"image/jpeg\",\"original_filename\":\"shelby-hughes-chonkchilla.jpg\",\"headers\":\"content-disposition: form-data; name=\\\"file\\\"; filename=\\\"shelby-hughes-chonkchilla.jpg\\\"\\r\\ncontent-type: image/jpeg\\r\\ncontent-length: 236098\\r\\n\"}"
 
-        file_data = JSON.parse(file_info)
-        file = ActionDispatch::Http::UploadedFile.new(
-          tempfile: "/spec/fixtures/files/shelby-hughes-chonkchilla.jpg",
-          type: file_data['content_type'],
-          filename: file_data['original_filename']
-          )
-          
-  require 'pry'; binding.pry
+        file = fixture_file_upload("spec/fixtures/files/shelby-hughes-chonkchilla.jpg", "image/jpeg")
+
+
         post "/api/v1/uploads", params: { id: "1", file: file }
+        # require 'pry'; binding.pry
 
         expect(response).to be_successful
         expect(response.status).to eq(200)
-        updated_user = JSON.parse(response.body, symbolize_names: true)
+      
 
-        expect(updated_user[:data]).to eq(1)
-        expect(updated_user[:data][:profile_image_link]).to eq("")
         
       end
     end
